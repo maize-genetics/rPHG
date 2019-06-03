54ae59d78b4047d0baabc41a038a649f7a895928
Brandon Monier <brandon.monier@gmail.com>
2019-06-03 16:07:43 -0400

    Initial commit

---
+++ R/graphBuilder.R
@@ -0,0 +1,17 @@
+#' @title Test PHG builder function
+#'
+#' @param configFile Configuration file for DB.
+#' @param myMethods Pairs of method calls - passed as string.
+#' @param myChrom List of chromosomes to include in graph. If NULL, defaults to all.
+#' @param myIncludeSequence Whether to include sequences in haplotype nodes. (ADVANCED)
+#' @param myIncludeVariant Whether to include variant contexts in haplotype nodes. (ADVANCED)
+#'
+#' @export
+graphBuilder <- function(configFile,
+                         myMethods,
+                         myChrom = NULL,
+                         myIncludeSequence = TRUE,
+                         myIncludeVariant = FALSE
+) {
+
+}
