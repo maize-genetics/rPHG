b3cc0bc13bb10f75214eb11b7c0600c97c4aab41
Brandon Monier <brandon.monier@gmail.com>
2019-06-04 14:53:39 -0400

    Format documentation; add placeholders

--- R/graphBuilder.R
+++ R/graphBuilder.R
@@ -1,17 +1,23 @@
 #' @title Test PHG builder function
 #'
+#' @description R wrapper to build a PHG graph object for downstream use.
+#'
 #' @param configFile Configuration file for DB.
 #' @param myMethods Pairs of method calls - passed as string.
-#' @param myChrom List of chromosomes to include in graph. If NULL, defaults to all.
-#' @param myIncludeSequence Whether to include sequences in haplotype nodes. (ADVANCED)
-#' @param myIncludeVariant Whether to include variant contexts in haplotype nodes. (ADVANCED)
+#' @param myChrom List of chromosomes to include in graph. If NULL, defaults
+#'   to all.
+#' @param myIncludeSequence Whether to include sequences in haplotype nodes.
+#'   (ADVANCED)
+#' @param myIncludeVariant Whether to include variant contexts in haplotype
+#'   nodes. (ADVANCED)
 #'
 #' @export
 graphBuilder <- function(configFile,
                          myMethods,
                          myChrom = NULL,
                          myIncludeSequence = TRUE,
-                         myIncludeVariant = FALSE
-) {
+                         myIncludeVariant = FALSE) {
 
+    ## Remove later
+    print("A temporary placeholder")
 }
