c46274db1d5662377ada08942e1d4a47e05f3143
Brandon Monier <brandon.monier@gmail.com>
2019-06-12 16:34:48 -0400

    Improve parameter names

--- R/graphBuilder.R
+++ R/graphBuilder.R
@@ -5,13 +5,13 @@
 #' @author Brandon Monier
 #' @author Peter Bradbury
 #'
-#' @param configFile Configuration file for graph database.
-#' @param myMethods Pairs of method calls - passed as string.
-#' @param myChrom List of chromosomes to include in graph. If NULL, defaults
+#' @param configFile Path to a configuration file for your graph database.
+#' @param methods Pairs of method calls - passed as string.
+#' @param chrom List of chromosomes to include in graph. If NULL, defaults
 #'   to all.
-#' @param myIncludeSequence Whether to include sequences in haplotype nodes.
+#' @param includeSequence Whether to include sequences in haplotype nodes.
 #'   (ADVANCED)
-#' @param myIncludeVariant Whether to include variant contexts in haplotype
+#' @param includeVariant Whether to include variant contexts in haplotype
 #'   nodes. (ADVANCED)
 #'
 #' @importFrom rJava .jnull
@@ -20,30 +20,33 @@
 #'
 #' @export
 graphBuilder <- function(configFile,
-                         myMethods,
-                         myChrom = NULL,
-                         myIncludeSequence = FALSE,
-                         myIncludeVariant = FALSE) {
+                         methods,
+                         chrom = NULL,
+                         includeSequence = FALSE,
+                         includeVariant = FALSE) {
 
     ## Create PHG plugin object
     phgPlugin <- rJava::new(
-        rJava::J("net.maizegenetics.pangenome.api.HaplotypeGraphBuilderPlugin"),
-        rJava::.jnull(),
+        rJava::J("net/maizegenetics/pangenome/api/HaplotypeGraphBuilderPlugin"),
+        rJava::.jnull("java/awt/Frame"),
         FALSE
     )
 
     ## Set parameters
-    phgPlugin$setParameter("configFile", configFile)
-    phgPlugin$setParameter("methods", myMethods)
-    phgPlugin$setParameter("includeSequences", toString(myIncludeSequence))
-    phgPlugin$setParameter("includeVariantContexts", toString(myIncludeVariant))
-    phgPlugin$setParameter("chromosomes", myChrom)
+    phgPlugin$configFile(configFile)
+    phgPlugin$methods(toString(methods))
+    phgPlugin$chromosomes(chrom)
+    # phgPlugin$includeSequences(toString(includeSequence))
+    # phgPlugin$includeVariantContexts(toString(includeVariant))
+    phgPlugin$setParameter("includeSequences", toString(includeSequence))
+    phgPlugin$setParameter("includeVariantContexts", toString(includeVariant))
+    # phgPlugin$setParameter("chromosomes", myChrom)
 
     ## Build the PHG...
     message("Building the graph...")
-    phgPlugin$build()
+    phgObj <- phgPlugin$build()
 
     ## Return PHG object
     message("Finished!")
-    return(phgPlugin)
+    return(phgObj)
 }
