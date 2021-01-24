69804afad7f8fada6230822aca1892b28b011364
Brandon <brandon.monier@gmail.com>
2021-01-24 15:24:32 -0500

    Add BrAPI connection class

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -10,3 +10,20 @@ setClass(
     Class = "PHGDataSet",
     contains = "RangedSummarizedExperiment"
 )
+
+
+#' @title BrapiPHG Class
+#'
+#' @description Class \code{BrapiPHG} defines a \code{rPHG}
+#'    Class for storing BrAPI connection data.
+#'
+#' @name BrapiPHG-class
+#' @rdname BrapiPHG-class
+#' @exportClass BrapiPHG
+setClass(
+    Class = "BrapiPHG",
+    representation = representation(
+        name = "character",
+        url = "character"
+    )
+)
