59acfe06c3b4a6153c41f564b9ff3e638e5ec422
Brandon <brandon.monier@gmail.com>
2021-03-12 17:01:35 -0500

    Initial commit

---
+++ R/brapi_utilities.R
@@ -0,0 +1,23 @@
+# === BrAPI utility and house-keeping methods =======================
+
+#' @title JSON to tibble converter
+#'
+#' @description Converts a requested JSON object to a \code{tibble}-based
+#'   \code{data.frame} object.
+#'
+#' @param object A \code{BrapiCon} object.
+#' @param ep A specified endpoint to request.
+#'
+#' @return A \code{tibble} object.
+#'
+#' @importFrom httr content
+#' @importFrom httr GET
+#' @importFrom jsonlite fromJSON
+#' @importFrom tibble as_tibble
+json2tibble <- function(object, ep) {
+    endPoint <- paste0(brapiURL(object), "/", ep)
+    res <- jsonlite::fromJSON(httr::content(httr::GET(endPoint), "text"))
+    return(tibble::as_tibble(res))
+}
+
+
