8b12f21119ac071e3ec4277013118818dbc88d57
Brandon <brandon.monier@gmail.com>
2023-11-08 09:46:13 -0500

    Manually set colors

--- R/anchor_dot_plot.R
+++ R/anchor_dot_plot.R
@@ -1,24 +1,24 @@
 ## ----
-#' @title 
+#' @title
 #' Generate dot plot from Anchor files
-#' 
+#'
 #' @description
 #' This function will generate a dot plot (collinearity) from an Anchor
-#' File generated from the program 
+#' File generated from the program
 #' [AnchorWave](https://github.com/baoxingsong/AnchorWave). This is one of the
 #' preliminary steps in creating a PHG database.
 #'
-#' @param anchorPath 
+#' @param anchorPath
 #' Path to anchor file.
-#' @param querySeqId 
+#' @param querySeqId
 #' Vector of sequence IDs (query).
-#' @param refSeqid 
+#' @param refSeqid
 #' Vector of sequence IDs (reference).
-#' @param queryLab 
+#' @param queryLab
 #' Optional label for query axis.
-#' @param refLab 
+#' @param refLab
 #' Optional label for reference axis.
-#' @param colorId 
+#' @param colorId
 #' How to color plots (\code{strand} or \code{score})
 #'
 #' @importFrom ggplot2 aes
@@ -40,40 +40,44 @@ anchorDotPlot <- function(
         refLab = NULL,
         colorId = c("strand", "score")
 ) {
-    
+
     if (is.null(queryLab)) queryLab <- "Query"
     if (is.null(refLab)) refLab <- "Reference"
-    
+
     colorId <- match.arg(colorId)
     if (colorId == "score") {
         scaleUnit <- ggplot2::scale_color_viridis_c()
-    } else {
-        scaleUnit <- NULL
+    } else if (colorId == "strand") {
+        scaleUnit <- ggplot2::scale_color_manual(
+            values = c(
+                "+" = "#DA897C",
+                "-" = "#0D6A82"
+            )
+        )
     }
-    
+
     tmpData <- read.table(file = anchorPath, head = TRUE)
-    
+
     if (!is.null(refSeqId)) tmpData <- tmpData[which(tmpData$refChr   %in% refSeqId), ]
     if (!is.null(querySeqId)) tmpData <- tmpData[which(tmpData$queryChr %in% querySeqId), ]
-    
+
     toMb <- function(x) x / 1e6
-    
-    
+
     p <- ggplot2::ggplot(data = tmpData) +
-        ggplot2::aes(x = queryStart, y = referenceStart, color = .data[[colorId]]) +
+        ggplot2::aes(x = queryStart, y = referenceStart, color = rlang::.data[[colorId]]) +
         ggplot2::geom_point(size = 0.3) +
         ggplot2::scale_y_continuous(labels = toMb) +
         ggplot2::scale_x_continuous(labels = toMb) +
         ggplot2::facet_grid(
-            rows = vars(refChr),
-            col = vars(queryChr),
+            rows   = ggplot2::vars(refChr),
+            col    = ggplot2::vars(queryChr),
             scales = "free",
-            space = "free"
+            space  = "free"
         ) +
         scaleUnit +
         ggplot2::xlab(paste(queryLab, "(Mbp)")) +
         ggplot2::ylab(paste(refLab, "(Mbp)"))
-    
+
     return(p)
 }
 
